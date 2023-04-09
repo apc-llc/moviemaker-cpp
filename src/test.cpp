0034d0e73722541abd1702e47474e22fcd4c6e09
Dmitry Mikushin <dmitry@kernelgen.org>
2023-04-09 16:21:05 +0200

    Splitting tests into read and write parts

--- src/test.cpp
+++ src/test.cpp
@@ -7,73 +7,67 @@
 
 using namespace std;
 
-TEST(movie, random_pixels)
+const unsigned int width = 1024;
+const unsigned int height = 768;
+const unsigned int nframes = 128;
+
+TEST(movie, random_pixels_write)
 {
-	const unsigned int width = 1024;
-	const unsigned int height = 768;
-	const unsigned int nframes = 128;
+	MovieWriter writer("random_pixels", width, height, 30);
 
+	vector<uint8_t> pixels(4 * width * height);
+	for (unsigned int iframe = 0; iframe < nframes; iframe++)
 	{
-		MovieWriter writer("random_pixels", width, height, 30);
-	
-		vector<uint8_t> pixels(4 * width * height);
-		for (unsigned int iframe = 0; iframe < nframes; iframe++)
-		{
-			for (unsigned int j = 0; j < height; j++)
-				for (unsigned int i = 0; i < width; i++)
-				{
-					pixels[4 * width * j + 4 * i + 0] = rand() % 256;
-					pixels[4 * width * j + 4 * i + 1] = rand() % 256;
-					pixels[4 * width * j + 4 * i + 2] = rand() % 256;
-					pixels[4 * width * j + 4 * i + 3] = rand() % 256;
-				}	
-	
-			writer.addFrame(&pixels[0]);
-		}
-	}
+		for (unsigned int j = 0; j < height; j++)
+			for (unsigned int i = 0; i < width; i++)
+			{
+				pixels[4 * width * j + 4 * i + 0] = rand() % 256;
+				pixels[4 * width * j + 4 * i + 1] = rand() % 256;
+				pixels[4 * width * j + 4 * i + 2] = rand() % 256;
+				pixels[4 * width * j + 4 * i + 3] = rand() % 256;
+			}	
 
-	{	
-		MovieReader reader("random_pixels", width, height);
-		MovieWriter writer("random_pixels2", width, height, reader.getFrameRate());
-
-		vector<uint8_t> pixels;	
-		while (1)
-		{
-			if (!reader.getFrame(pixels))
-				break;
-		
-			writer.addFrame(&pixels[0]);
-		}
+		writer.addFrame(&pixels[0]);
 	}
 }
 
-TEST(movie, svg_image)
+TEST(movie, random_pixels_read)
 {
-	const unsigned int width = 1024;
-	const unsigned int height = 768;
-	const unsigned int nframes = 128;
+	MovieReader reader("random_pixels", width, height);
+	MovieWriter writer("random_pixels2", width, height, reader.getFrameRate());
 
+	vector<uint8_t> pixels;	
+	while (1)
 	{
-		MovieWriter movie("svg_image", width, height);
-
-		for (unsigned int iframe = 0; iframe < nframes; iframe++)
-		{
-			movie.addFrame(TIGER_SVG);
-		}
+		if (!reader.getFrame(pixels))
+			break;
+	
+		writer.addFrame(&pixels[0]);
 	}
+}
 
-	{	
-		MovieReader reader("svg_image", width, height);
-		MovieWriter writer("svg_image2", width, height);
+TEST(movie, svg_image_write)
+{
+	MovieWriter movie("svg_image", width, height);
 
-		vector<uint8_t> pixels;	
-		while (1)
-		{
-			if (!reader.getFrame(pixels))
-				break;
-		
-			writer.addFrame(&pixels[0]);
-		}
+	for (unsigned int iframe = 0; iframe < nframes; iframe++)
+	{
+		movie.addFrame(TIGER_SVG);
+	}
+}
+
+TEST(movie, svg_image_read)
+{	
+	MovieReader reader("svg_image", width, height);
+	MovieWriter writer("svg_image2", width, height);
+
+	vector<uint8_t> pixels;	
+	while (1)
+	{
+		if (!reader.getFrame(pixels))
+			break;
+	
+		writer.addFrame(&pixels[0]);
 	}
 }
 
